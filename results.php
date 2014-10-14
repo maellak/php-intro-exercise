58061f29b32caba8041a25a174dfc3ba583a67b6
frostig <lena-simfo@hotmail.com>
2014-10-14 22:44:47 +0300

    results.php

---
+++ results.php
@@ -0,0 +1,11 @@
+<?php
+$movies=array("Drama"=>array("The Shawshank Redemption","The Green Mile","Requiem for a Dream"),"Comedy"=>array("The Hangover","Knocked Up","Due Date"),"Horror"=>array("The Shining","Halloween","The Conjuring"));
+
+if ($_REQUEST['']){
+
+
+	
+}
+
+
+?>
