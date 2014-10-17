6b0f54f5e278b46c5a9b05bd5315314b5035a694
Katiforis Giorgos <eraskino@gmail.com>
2014-10-17 22:39:35 +0300

    ask1
    ask1

---
+++ check.js
@@ -0,0 +1,18 @@
+function checkfield()  
+{  
+var empty = "";
+var field = document.getElementById["movie-name"].value; 
+ 
+alert("Please type the name of the movie"); 
+if (field == ""))  
+{  
+
+alert("Please type the name of the movie");  
+return false;  
+}  
+else   
+{  
+return true;   
+}  
+}  
+
